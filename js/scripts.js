584ed65d0452a65df52b5b6fe72bbefa5a73a8e2
hanmoyang <runlin@RunlindeMBP.fios-router.home>
2017-04-13 18:37:18 -0400

    With markers

--- js/scripts.js
+++ js/scripts.js
@@ -5,131 +5,145 @@ var map = L.map('mapContainer').setView([40.745653, -73.989614], 12);
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'
 }).addTo(map);
 
-L.geoJson(universities,{
-  style:function(feature){
-    
-    var customColor;
+    var universities=L.geoJson(universities,{
+      style:function(feature){
+        
+        var customColor;
 
-    if(feature.properties.value == 1){
-      customColor="orange";
-    }
+        if(feature.properties.value == 1){
+          customColor="orange";
+        }
 
-    if(feature.properties.value == 2){
-      customColor="blue";
-    }
+        if(feature.properties.value == 2){
+          customColor="blue";
+        }
 
-    if(feature.properties.value == 3){
-      customColor="purple";
-    }    
+        if(feature.properties.value == 3){
+          customColor="purple";
+        }    
 
-    if(feature.properties.value == 4){
-      customColor="yellow";  
-    }    
+        if(feature.properties.value == 4){
+          customColor="yellow";  
+        }    
 
-    return {
-      color:customColor,
-      fillColor:customColor,
-      weight:2,
+        return {
+          color:customColor,
+          fillColor:customColor,
+          weight:2,
+        };
+      }
+    }).addTo(map);
+
+    var museumMarker = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
     };
-  },
-}).addTo(map);
 
-  var museumIcon = L.icon({
-    iconUrl: 'museumicon.png'
-  });
+    var galleryMarker = {
+        radius: 8,
+        fillColor: "red",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
 
-  var galleryIcon = L.icon({
-    iconUrl:'galleryicon.png'
-  })
+      var museums=L.geoJson(museums,{
+        pointToLayer: function (feature, latlng) {
+         var geojsonMarkerOptions = {
+            radius: 5,
+            fillColor: "deepskyblue",
+            color: "white",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8,
+            z: 1
+        };
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+      }
+      
+        }).addTo(map);
+
+      var galleries=L.geoJson(galleries,{
+        pointToLayer: function (feature, latlng) {
+         var geojsonMarkerOptions = {
+            radius: 5,
+            fillColor: "red",
+            color: "white",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8,
+            z: 1
+        };
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+      }        
+      }).addTo(map);
 
 
-  var museums=L.geoJson(museums, {
-    onEachFeature: function (feature, layer) {
-      layer.on('click', function() {
+    var museumsLayerGroup= L.layerGroup([museums]);
+    var galleriesLayerGroup = L.layerGroup([galleries]);
+    var universitiesLayerGroup = L.layerGroup([universities]);
 
-        var string='<h2>'
-        +feature.properties.NAME
-        +'</h2><div>'
-        +feature.properties.TEL
-        +'</div><div>'
-        +feature.properties.URL
-        +'</div><div>'
-        +feature.properties.ADDRESS1
-        +'</div><div>'
-        +feature.properties.CITY
-        +'</div><div>'
-        +feature.properties.ZIP
-        +'</div>'
+        
+     museumsLayerGroup.addTo(map);
+     galleriesLayerGroup.addTo(map);
+     
+     var overlays = {
+      "Universities":universities
+     }
 
-        $('#sidebar h2').html(string)
-      })
-    },
+     var types = {
+       "Museums": museumsLayerGroup,
+       "Galleries": galleriesLayerGroup,
+     }
 
-    pointTolayer:function(feature,latLng){
-     return L.marker(latLng,{icon:museumIcon}).addTo(map);
-    }
+
+     L.control.layers(types,overlays, {
+      collapsed: false,
+     }).addTo(map);
+
+
+
+    $('.a').on('click',function(){
+      map.flyTo([40.861992, -73.885656],13)
+    });
+
+    $('.b').on('click',function(){
+      map.flyTo([40.807934, -73.962359],13)
+    });
+
+    $('.c').on('click',function(){
+      map.flyTo([40.729196, -73.996000],13)
+    });
+
+    $('.d').on('click',function(){
+      map.flyTo([40.735463, -73.994614],15)
+    });
+
+    $('.reset').on('click',function(){
+      map.flyTo([40.735021, -73.994787], 11)
+    });
+
+    var museuminfo = L.geoJson(museums, {
+      opacity: 0,
+      fillOpacity: 0
+    }).bindPopup(function (layer) {
+        return (' Museum Name: ' + layer.feature.properties.NAME +
+            '</br> Tel: ' + layer.feature.properties.TEL+
+            '</br> Address: ' + layer.feature.properties.ADDRESS1  +
+            '</br> City: ' + layer.feature.properties.CITY);
     }).addTo(map);
 
-  var galleries=L.geoJson(galleries, {
-      onEachFeature: function (feature, layer) {
-        layer.on('click', function() {
-
-        var string='<h2>'
-        +feature.properties.NAME
-        +'</h2><div>'
-        +feature.properties.TEL
-        +'</div><div>'
-        +feature.properties.URL
-        +'</div><div>'
-        +feature.properties.ADDRESS1
-        +'</div><div>'
-        +feature.properties.CITY
-        +'</div><div>'
-        +feature.properties.ZIP
-        +'</div>'
-
-        $('#sidebar h2').html(string)
-        })
-      },
-
-      pointTolayer:function(feature,latLng){
-      return L.marker(latLng,{icon:galleryIcon}).addTo(map);
-    }
-    }).addTo(map);
-
-
-  var museumsLayerGroup= L.layerGroup([museums]);
-  var galleriesLayerGroup = L.layerGroup([galleries]);
-
-    
-   museumsLayerGroup.addTo(map);
-   galleriesLayerGroup.addTo(map);
-
-   var types = {
-     "Museums": museumsLayerGroup,
-     "Galleries": galleriesLayerGroup,
-   }
-
-   L.control.layers(null, types, {
-    collapsed: false
-   }).addTo(map);
-
-  $('.a').on('click',function(){
-    map.flyTo([40.861992, -73.885656],13)
-  });
-
-  $('.b').on('click',function(){
-    map.flyTo([40.807934, -73.962359],13)
-  });
-
-  $('.c').on('click',function(){
-    map.flyTo([40.729196, -73.996000],13)
-  });
-
-  $('.d').on('click',function(){
-    map.flyTo([40.735463, -73.994614],15)
-  });
-
-  $('.reset').on('click',function(){
-    map.flyTo([40.735021, -73.994787], 11)
-  });
+    var museuminfo = L.geoJson(galleries, {
+      opacity: 0,
+      fillopacity: 0
+    }).bindPopup(function (layer) {
+        return (' Museum Name: ' + layer.feature.properties.NAME +
+            '</br> Tel: ' + layer.feature.properties.TEL+
+            '</br> Address: ' + layer.feature.properties.ADDRESS1  +
+            '</br> City: ' + layer.feature.properties.CITY);
+    }).addTo(map);    
